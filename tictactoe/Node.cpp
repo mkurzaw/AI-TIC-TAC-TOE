74b73a0337fb00cc3701a88c9907d8be174d530c
mkurzaw <62571973+mkurzaw@users.noreply.github.com>
2022-01-13 17:37:45 +0100

    Add files via upload

---
+++ tictactoe/Node.cpp
@@ -0,0 +1,140 @@
+#include "Node.h"
+#include "Utility.h"
+
+using namespace std;
+
+char Node::computer = ' ';
+unsigned int Node::depth = 0;
+unsigned int Node::max_depth = 7;
+
+Node::Node(vector<vector<char>> state, unsigned int level, char current_player)
+{
+    this->level = level;
+    this->current_player = current_player;
+    this->state = state;
+    this->value = TIE;
+//kolejny ruch zwieksza glebokosc drzewa o 1
+    if (level > Node::depth)
+    {
+        Node::depth = level;
+    }
+
+    if (win(state))
+    {
+        //jezeli ruch gracza to przegral
+        if (current_player != Node::computer)
+        {
+            this->value = WIN - level + 3;
+            return;
+        }
+        this->value = LOST + level - 3;
+        return;
+    }
+    //remis nic nie robimy bo wartosc startowa i tak 0
+    else if (tie(state))
+    {
+        return;
+    }
+    //przewidywanie ruchow do przodu jesli przekroczy to zakoncz
+    if (level == Node::max_depth)
+    {
+        return;
+    }
+//petla sprawdza ktore pole jest puste i tworzy dla niego kolejne mozliwosci
+    for (int i = 0; i < state.size(); i++)
+    {
+        for (int j = 0; j < state.size(); j++)
+        {
+            //jezeli pusty tworzymy nowy stan
+            if (state.at(i).at(j) == ' ')
+            {
+                vector<vector<char>> new_state = state;
+                new_state.at(i).at(j) = current_player;
+                //czy current player=x, jesli tak zwroc o jesli nie x tak aby zmienic symbol w nastepnych ruchach
+                Node new_node(new_state, level+1, current_player == 'x' ? 'o' : 'x');
+                //dodanie kolejnego dziecka na liste
+                children.emplace_back(new_node);
+            }
+        }
+    }
+}
+
+unsigned int Node::getLevel() const
+{
+    return level;
+}
+
+void Node::calculateValue(int& alphaBeta, const bool first)
+{
+    if (!children.empty())
+    {
+        value = children.at(0).value;
+        if (current_player == Node::computer)
+        {
+            //jezeli wartosc obecna jest wieksza niz alfabeta to bierzemy dziecko
+            for (auto & child : children)
+            {
+
+                if (!first && child.value > alphaBeta)
+                {
+                    value = child.value;
+                    return;
+                }
+                else if (child.value > value)
+                {
+                    value = child.value;
+                }
+            }
+            if (value < alphaBeta)
+            {
+                alphaBeta = value;
+            }
+        }
+        else
+        {
+            for (auto & child : children)
+            {
+                if (!first && child.value < alphaBeta)
+                {
+                    value = child.value;
+                    return;
+                }
+                if (child.value < value)
+                {
+                    value = child.value;
+                }
+            }
+            if (value  > alphaBeta)
+            {
+                alphaBeta = value;
+            }
+        }
+    }
+}
+
+int Node::getValue() const
+{
+    return value;
+}
+
+vector<vector<char>> Node::getState() const
+{
+    return state;
+}
+
+vector<Node> Node::getChildren() const
+{
+    return children;
+}
+
+void Node::getAdresses(vector<Node*>& v, Node* root)
+{
+    if (!root->children.empty())
+    {
+        for (auto & i : root->children)
+        {
+            getAdresses(v, &i);
+        }
+    }
+    v.emplace_back(root);
+}
---
+++ tictactoe/main.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <vector>
+#include <random>
+#include "Point.h"
+#include "Utility.h"
+
+#define DEPTH 4 // Ilosc przewidywanych rychow do przodu
+
+using namespace std;
+
+int main()
+{
+    //podaje wielkosc pola
+    size_t field_size;
+    get_input("Podaj rozmiar pola: ", field_size);
+    // wprowadza ilosc znakow w rzêdzie
+    size_t fields_count = 0;
+    while (fields_count < 3)
+    {
+        get_input("Podaj ilosc znakow w rzedzie: ", fields_count);
+    }
+//tworze dwuwymiarowa tablice wektorów i wypelnia j¹ spacjami
+    vector<vector<char>> symbols;
+    fill_vector(symbols, fields_count, ' ');
+//losowanie czy jeestes x czy o
+    random_device rd;
+    mt19937 mt(rd());
+    uniform_int_distribution<int> dist(0, 1);
+
+    auto player = (int) dist(mt);
+    //wypisywanie symbolu ktorym gramy
+    cout << "Gracz: " << (player == 1 ? 'x' : 'o') << endl << "Komputer: " << (player == 1 ? 'o' : 'x') << endl;
+    if (player == 1)
+    {
+        bot('o', symbols, 4);
+    }
+
+    while (true)
+    {
+        //wywietlanie pola do gry
+        print_map(fields_count, symbols, field_size);
+        cout << "#### RUCH GRACZA ####" << endl;
+        //wczyranie puntu wybranego gdy jest wolny
+        Point p;
+        do
+        {
+            p = Point::get_point("Podaj x y: ");
+        }
+        while (p.getX() >= fields_count || p.getY() >= fields_count || symbols.at(p.getY()).at(p.getX()) != ' ');
+        //wpisuje x lub o w zaleznosci od gracza na plansze
+        symbols.at(p.getY()).at(p.getX()) = player == 1 ? 'x' : 'o';
+        print_map(fields_count, symbols, field_size);
+        if (win(symbols))
+        {
+            cout << "Wygral gracz!" << endl;
+            break;
+        }
+        else if (tie(symbols))
+        {
+            cout << "Remis!";
+            break;
+        }
+
+        cout << "#### RUCH BOTA ####" << endl;
+        //zostahe wykonany ruch bota
+        bot(player == 1 ? 'o' : 'x', symbols, DEPTH);
+        if (win(symbols))
+        {
+            print_map(fields_count, symbols, field_size);
+            cout << "Wygral bot!" << endl;
+            break;
+        }
+        else if (tie(symbols))
+        {
+            print_map(fields_count, symbols, field_size);
+            cout << "Remis!";
+            break;
+        }
+    }
+}
+
